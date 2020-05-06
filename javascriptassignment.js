5dce1833ae63a482c7b22c85320c0b7fc0a143d2
bikash1401297185 <bikashdhangada.majhi@zemosolabs.com>
2020-05-06 13:27:47 +0530

    Added javascript assignment

--- javascriptassignment.js
+++ javascriptassignment.js
@@ -3,12 +3,10 @@ var fruits=[{name: "strawberry", quantity: "15", priceperunit: 10},
 {name: "apple", quantity: "6", priceperunit: 20},
 {name: "mango", quantity: "12", priceperunit: 50},
 {name: "pear", quantity: "10", priceperunit: 80}]
-//console.log(fruits.length)
 
 for(var i=0;i<fruits.length;i++)
 {
   var prod=fruits[i].priceperunit * fruits[i].quantity
-  //console.log(prod)
   if(prod>250)
   {
   console.log(fruits[i].name)
