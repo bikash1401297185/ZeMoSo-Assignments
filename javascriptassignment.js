6444e258a67e7036d683ffeb8e68d1db904a848e
bikash1401297185 <bikashdhangada.majhi@zemosolabs.com>
2020-05-06 13:43:11 +0530

    Removed double quotes

--- javascriptassignment.js
+++ javascriptassignment.js
@@ -1,8 +1,8 @@
 var fruits=[{name: "strawberry", quantity: "15", priceperunit: 10},
-{name: "pineapple", quantity: "10", priceperunit: 40},
-{name: "apple", quantity: "6", priceperunit: 20},
-{name: "mango", quantity: "12", priceperunit: 50},
-{name: "pear", quantity: "10", priceperunit: 80}]
+{name: "pineapple", quantity: 10, priceperunit: 40},
+{name: "apple", quantity: 6, priceperunit: 20},
+{name: "mango", quantity: 12, priceperunit: 50},
+{name: "pear", quantity: 10, priceperunit: 80}]
 
 for(var i=0;i<fruits.length;i++)
 {
